8916e97de1fd3e57e1d30e508672fe24d5867de2
Haseeb Anwar <haseebanwar99@gmail.com>
2020-06-07 10:34:55 +0500

    Updated blog posts, added new skill

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.28c4dc94dac6b7969058f1da8718aa88.js"
+  "/precache-manifest.47b8bec53c83d0fb885c8290899bdaa3.js"
 );
 
 self.addEventListener('message', (event) => {
