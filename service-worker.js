2035f27cf9594cf22d4aa9222655353707f8251b
Haseeb Anwar <haseebanwar99@gmail.com>
2021-09-23 10:46:07 +0500

    Added GraphQL in Expertise and Resume

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b4bda25338f6b81eea9cb3d92c3c63d7.js"
+  "/precache-manifest.563f7d27c8379e245fbd13bd82e8d8ed.js"
 );
 
 self.addEventListener('message', (event) => {
